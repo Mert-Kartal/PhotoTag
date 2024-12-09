10beefa091b84795d4b8edc4261ba1178b9954b4
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2024-12-09 18:46:42 +0300

    localStorage

--- main.js
+++ main.js
@@ -1,11 +1,68 @@
 "use strict";
 
-let createPin = document.getElementById("createPin");
-let locatePin = document.getElementById("locatePin");
-let contain = document.getElementById("contain");
+const createPin = document.getElementById("createPin");
+const locatePin = document.getElementById("locatePin");
+const contain = document.getElementById("contain");
+const photo = document.getElementById("photo");
 let isLocated;
-let arr = [];
+let arr = JSON.parse(localStorage.getItem("pins")) || [];
 let pin;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (arr.length > 0) {
+    arr.forEach((item) => {
+      isLocated = false;
+      pin = document.createElement("div");
+      pin.id = item.hashtag;
+      pin.setAttribute("desc", item.description);
+      pin.style.left = `${item.xPosition}px`;
+      pin.style.top = `${item.yPosition}px`;
+      pin.classList.add("popStyle");
+      pin.addEventListener("mouseover", function () {
+        console.log(
+          `Hashtag: ${this.id}, Description: ${this.getAttribute("desc")}`
+        );
+        this.innerHTML = `
+        <div class="popover">
+            <div id="hashtagPlace" class="p-1 rounded-top-2">${this.id}</div>
+            <div id="descpPlace" class="p-1 rounded-bottom-2">${this.getAttribute(
+              "desc"
+            )}</div>
+        </div>
+        `;
+      });
+      pin.addEventListener("mouseout", function () {
+        this.innerHTML = ``;
+      });
+      pin.addEventListener("click", function () {
+        let deletePin = confirm("Silmek istediğinizden emin misiniz?");
+        if (deletePin) {
+          this.remove();
+          arr = arr.filter((item) => this.id !== item.hashtag);
+          localStorage.setItem("pins", JSON.stringify(arr));
+          console.log(arr);
+          alert(`${this.id} kaldırıldı;`);
+        } else {
+          alert("Silme işlemi iptal edildi");
+        }
+      });
+      isLocated = true;
+      contain.appendChild(pin);
+    });
+  } else {
+    console.log("Hiç pin bulunamadı.");
+  }
+});
+
+photo.addEventListener("mouseover", () => (photo.style.cursor = "pointer"));
+
+photo.addEventListener("click", (e) => {
+  let xPosition = e.offsetX;
+  let yPosition = e.offsetY;
+  console.log(xPosition, yPosition);
+  document.getElementById("xPosition").value = xPosition;
+  document.getElementById("yPosition").value = yPosition;
+});
 
 createPin.addEventListener("click", () => {
   let hashtagValue = document.getElementById("hashtag").value;
@@ -25,11 +82,39 @@ createPin.addEventListener("click", () => {
       (item) => item.hashtag === hashtagValue.trim()
     );
     if (searchHashtag) {
-      alert("Bu değeri önceden girdiniz");
-      hashtagValue = "";
+      alert(`${hashtagValue} değerini daha önceden kullandınız`);
+      document.getElementById("hashtag").value = "";
+      document.getElementById("description").value = "";
+      document.getElementById("xPosition").value = "";
+      document.getElementById("yPosition").value = "";
     } else {
-      alert("Pin oluşturuldu!");
-      isLocated = false;
+      let searchPosition = arr.some(
+        (item) =>
+          item.xPosition === xPositionValue.trim() &&
+          item.yPosition === yPositionValue.trim()
+      );
+      if (searchPosition) {
+        alert(`Bu konuma daha öncesinde pin yerleştirdiniz.`);
+        document.getElementById("hashtag").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("xPosition").value = "";
+        document.getElementById("yPosition").value = "";
+      } else {
+        if (
+          xPositionValue.trim() > 500 ||
+          yPositionValue.trim() > 750 ||
+          (xPositionValue.trim() || yPositionValue.trim()) < 0
+        ) {
+          alert(`Fotoğrafın boyutundan farklı değer girdiniz`);
+          document.getElementById("hashtag").value = "";
+          document.getElementById("description").value = "";
+          document.getElementById("xPosition").value = "";
+          document.getElementById("yPosition").value = "";
+        } else {
+          alert("Pin oluşturuldu!");
+          isLocated = false;
+        }
+      }
     }
   }
 });
@@ -66,6 +151,7 @@ locatePin.addEventListener("click", () => {
         yPosition: yPositionValueTrim,
       };
       arr.push(newPin);
+      localStorage.setItem("pins", JSON.stringify(arr));
       newPin = {};
 
       document.getElementById("hashtag").value = "";
@@ -94,6 +180,7 @@ locatePin.addEventListener("click", () => {
         if (deletePin) {
           this.remove();
           arr = arr.filter((item) => this.id !== item.hashtag);
+          localStorage.setItem("pins", JSON.stringify(arr));
           console.log(arr);
           alert(`${this.id} kaldırıldı;`);
         } else {
@@ -109,7 +196,6 @@ locatePin.addEventListener("click", () => {
 /*
  To Do
  preview in a page
- save local storage
  input image file
- X and Y coordinations
+ what if user create pin but dont locate and then try to create same 
 */
